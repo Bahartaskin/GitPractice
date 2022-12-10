3a9f89737b9e5339021cdc60ac1d23620ede34b8
Bahar Taskin <cakirbhr7@gmail.com>
2022-12-10 18:49:27 +0300

    created Test1 class

---
+++ src/pac1/Test1.java
@@ -0,0 +1,7 @@
+package pac1;
+
+public class Test1 {
+    // to add the files into the staging area
+    //right-click on the file->go to Git->go to add->click to add
+    //for multiple files right click on Package
+}

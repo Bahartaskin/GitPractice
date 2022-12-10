7cfd9ffbb93b52028e1823c2c8dccff0f0697497
Bahar Taskin <cakirbhr7@gmail.com>
2022-12-10 19:18:49 +0300

    updated Test1 class

--- src/pac1/Test1.java
+++ src/pac1/Test1.java
@@ -1,6 +1,14 @@
 package pac1;
 
 public class Test1 {
+
+    /*
+    1- create a file
+    2- add the file to staging area
+    3- commit your file one by one with explanation
+    4- push your file from local master to remote(origin) master
+     */
+
     // to add the files into the staging area
     //right-click on the file->go to Git->go to add->click to add
     //for multiple files right click on Package

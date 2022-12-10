e24df804bf01b2300fa7dceb513c1dae4e9b6443
Bahar Taskin <cakirbhr7@gmail.com>
2022-12-10 19:21:11 +0300

    updated Test1 class 2nd time

--- src/pac1/Test1.java
+++ src/pac1/Test1.java
@@ -3,6 +3,8 @@ package pac1;
 public class Test1 {
 
     /*
+    first flow
+    file->staging area->local repository->remote repository
     1- create a file
     2- add the file to staging area
     3- commit your file one by one with explanation
